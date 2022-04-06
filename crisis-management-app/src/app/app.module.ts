d31947d33ca9fb2f75f5406f8307a4e288927067
Tomek Michniewicz <michniewicz.tomasz@gmail.com>
2022-04-06 10:46:08 -0700

    refactor: added navigation buttons

--- crisis-management-app/src/app/app.module.ts
+++ crisis-management-app/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+
+import { MapsModule } from './shared/maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListViewComponent } from './list-view/list-view.component';
 import { MapViewComponent } from './map-view/map-view.component';
 import { DetailsViewComponent } from './details-view/details-view.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MapsModule } from './shared/maps';
 import { InformationViewComponent } from './information-view/information-view.component';
 
 registerLocaleData(en);
@@ -29,12 +32,15 @@ registerLocaleData(en);
   ],
   imports: [
     BrowserModule,
-    NzButtonModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+
+    NzButtonModule,
+    NzRadioModule,
+
     MapsModule,
+    AppRoutingModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
