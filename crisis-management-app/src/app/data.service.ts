bd1c37ba24e4443a7f71048a797bf57b0f56d3f4
Tomek Michniewicz <michniewicz.tomasz@gmail.com>
2022-04-06 10:36:10 -0700

    refactor: details view to get/display full data

--- crisis-management-app/src/app/data.service.ts
+++ crisis-management-app/src/app/data.service.ts
@@ -13,4 +13,9 @@ export class DataService {
   getData(): Observable<Information[]> {
     return of(INFORMATIONS);
   }
+
+  getDataById(id: string): Promise<Information> {
+    const result = INFORMATIONS.find(item => item.id === id);
+    return (result) ? Promise.resolve(result) : Promise.reject(`Item not found: ${id}`);
+  }
 }
--- crisis-management-app/src/app/details-view/details-view.component.ts
+++ crisis-management-app/src/app/details-view/details-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { Information } from '../models';
 
 @Component({
   selector: 'app-details-view',
@@ -9,13 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsViewComponent implements OnInit {
 
   id?: string;
+  data?: Information;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private dataService: DataService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       // tslint:disable-next-line:no-string-literal
       this.id = params['id'];
+
+      if (this.id) {
+        this.dataService.getDataById(this.id).then(data => {
+          this.data = data;
+        });
+      }
     });
   }
 }
