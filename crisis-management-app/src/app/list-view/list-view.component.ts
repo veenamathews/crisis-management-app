f874664c627a872f6f8954e37276309db77e08df
AVANADE-CORP\veena.mathews <veena.mathews@avanade.com>
2022-04-12 20:58:44 -0400

    other views

--- crisis-management-app/src/app/list-view/list-view.component.ts
+++ crisis-management-app/src/app/list-view/list-view.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Information } from '../models';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { InformationNode } from '../models/Information';
+import {FlatTreeControl} from '@angular/cdk/tree';
 
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
@@ -9,6 +17,25 @@ import { Information } from '../models';
 })
 export class ListViewComponent implements OnInit {
 
+  private _transformer = (node: InformationNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+  
   data?: Information[];
   showOriginalData = false;
   categoryList: any[] = [];
@@ -18,12 +45,19 @@ export class ListViewComponent implements OnInit {
   };
   constructor(public dataService: DataService) { }
 
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+ 
+
+
   ngOnInit(): void {
     this.dataService.messages$.subscribe(data => {
       this.data = data;
     });
-    this.dataService.getCategoryList().then(data => {
-      this.categoryList = data;
-    })
+    this.categoryList = this.dataService.knownCategory;
+    console.log(this.categoryList)
   }
+
+
+
+
 }
