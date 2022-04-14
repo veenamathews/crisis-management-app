af211311d4803ff452e95d525c5eaaafd51449ea
AVANADE-CORP\veena.mathews <veena.mathews@avanade.com>
2022-04-14 18:48:33 -0400

    fix up filter

--- crisis-management-app/src/app/data.service.ts
+++ crisis-management-app/src/app/data.service.ts
@@ -14,76 +14,74 @@ export class DataService {
 
   private messagesSubject = new BehaviorSubject<Information[]>(MESSAGES_DATASET_002);
   messages$ = this.messagesSubject.asObservable();
-  private masterMessage: Information[] = MESSAGES_DATASET_002;
+  private masterMessageSubject = new BehaviorSubject<Information[]>(MESSAGES_DATASET_002);
+  mastermessages$ = this.messagesSubject.asObservable();
 
   knownTags = [
     {
       name: 'Food',
       color: '#37b24d',
       icon: 'fastfood',
-      checked: false
+      checked: true
     },
     {
       name: 'Shelter',
       color: '#f08c00',
       icon: 'hotel',
-      checked: false
+      checked: true
     },
     {
       name: 'Health Services',
       color: '#d6336c',
       icon: 'local_hospital',
-      checked: false
+      checked: true
     },
     {
       name: 'Transportation',
       color: '#1c7ed6',
       icon: 'directions_bus_filled',
-      checked: false
+      checked: true
     },
     {
       name: 'Translation',
       color: '#ae3ec9',
       icon: 'interpreter_mode',
-      checked: false
+      checked: true
     },
     {
       name: 'Legal',
       color: '#22b8cf',
       icon: 'gavel',
-      checked: false
+      checked: true
     },
     {
       name: 'Volunteering',
       color: '#20c997',
       icon: 'sports_kabaddi',
-      checked: false
+      checked: true
     },
     {
       name: 'Volunteers Needed',
       color: '#f06595',
       icon: 'group_add',
-      checked: false
+      checked: true
     },
     {
       name: 'Other',
       color: '#adb5bd',
       icon: 'info',
-      checked: false
+      checked: true
     },
   ];
 
   constructor(private http: HttpClient) { }
 
   filter(checkedList: string[]): void {
-    const filteredMessages = this.messagesSubject.value.filter(item => item.category === "Food");
-
+    let filteredMessages: any = [];
     checkedList.forEach(element => {
-      this.messagesSubject.value.filter(item => item.category === element);
+      filteredMessages = filteredMessages.concat([...this.masterMessageSubject.value.filter(item => item.category == element)]);
     });
-
-    const filteredMessages1 = this.messagesSubject;
-    this.messagesSubject.next(filteredMessages1.value);
+    this.messagesSubject.next(filteredMessages);
   }
 
   getDataById(id: string): Promise<Information> {
